d2adb2eacba612d8490eeb92e4bce088e3801956
KCBYO <keepcalmandbyo@gmail.com>
2022-05-06 11:32:11 -0500

    Make wait time based on time elapsed since last wait

--- src/wait.rs
+++ src/wait.rs
@@ -1,25 +1,34 @@
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
-#[derive(Clone, Copy, Debug)]
+const DEFAULT_WAIT_TIME: u64 = 500;
+
+#[derive(Clone, Debug)]
 pub struct Waiter {
     duration: Duration,
+    epoch: Instant,
     should_wait: bool,
 }
 
 impl Waiter {
     pub fn new() -> Self {
         Waiter {
-            duration: Duration::from_millis(250),
+            duration: Duration::from_millis(DEFAULT_WAIT_TIME),
+            epoch: Instant::now(),
             should_wait: false,
         }
     }
 
     pub fn wait(&mut self) {
         if self.should_wait {
-            std::thread::sleep(self.duration);
+            let now = Instant::now();
+            let diff = now - self.epoch;
+            if diff < self.duration {
+                std::thread::sleep(self.duration - diff);
+            }
         } else {
             self.should_wait = true;
         }
+        self.epoch = Instant::now();
     }
 }
 
