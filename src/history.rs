fec3ee522539d638bb68d69736101fe2ebda8266
KCBYO <keepcalmandbyo@gmail.com>
2022-05-27 00:40:40 -0500

    Attempt to solve date filter issue

--- src/history.rs
+++ src/history.rs
@@ -5,7 +5,7 @@ use std::{
     path::PathBuf,
 };
 
-use chrono::{Date, DateTime, Utc};
+use chrono::{Date, DateTime, Utc, Duration};
 use directories::UserDirs;
 use hashbrown::HashSet;
 use regex::Regex;
@@ -66,11 +66,17 @@ impl History {
     }
 
     pub fn write(&self, limit: Date<Utc>) -> io::Result<()> {
+        // So, regarding the bug report from 26 May 2022, here's the plan: we're going to keep
+        // history items a *full day* beyond the filter date. This is on the assumption that there
+        // is some kind of day/hour tomfoolishness going on. Adding some slack here will cost
+        // almost nothing and potentially solve this issue without any further research required.
+        
+        let slack_date = safe_decrement_limit(limit);
         let entries: HashSet<_> = self
             .entries
             .iter()
             .cloned()
-            .filter(|entry| entry.date >= limit)
+            .filter(|entry| entry.date >= slack_date)
             .collect();
 
         let new_history = get_new_history_path()?;
@@ -124,4 +130,19 @@ fn deserialize_date<'de, D: Deserializer<'de>>(deserializer: D) -> Result<Date<U
 fn serialize_date<S: Serializer>(date: &Date<Utc>, serializer: S) -> Result<S::Ok, S::Error> {
     let datetime = date.and_hms(0, 0, 0);
     datetime.serialize(serializer)
+}
+
+fn safe_decrement_limit(limit: Date<Utc>) -> Date<Utc> {
+    // The plan here is to decrement limit by 24 hours and, if that proves insufficient to
+    // decrease the day by one, to decrement limit by 25 hours instead. WHETHER OR NOT ANY OF THIS
+    // MATTERS is a matter of complete speculation. There is API space in dotnet to cover this
+    // case in an affirmative way (think AddDays(n)/SubtractDays(m)), but the situation here is,
+    // at least for me, ambiguous with regard to time changes and leap seconds and all that shit.
+
+    let result = limit - Duration::hours(24);
+    if result == limit {
+        limit - Duration::hours(25)
+    } else {
+        result
+    }
 }
